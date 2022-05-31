41ebd3e081ccf46cf75690fe6553dae8a3eb782d
ronaldherrera <ronaldcalzadilla31@gmail.com>
2022-05-31 16:36:08 +0200

    create login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,3 +1,58 @@
+import { useContext, useState } from "react";
+import { logInUserService } from "../services/loginUserService";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
 export const LoginPage = () => {
-  return <p>Estamos en el login</p>;
+  const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleForm = async (e) => {
+    e.preventDefault();
+
+    try {
+      const token = await logInUserService({ email, password });
+
+      login(token);
+      console.log(token);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  return (
+    <section>
+      <h1>Logikkkkkkn</h1>
+      <form onSubmit={handleForm}>
+        <fieldset>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="pass">Password</label>
+          <input
+            type="password"
+            name="pass"
+            id="pass"
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </fieldset>
+
+        <button>Login</button>
+        {error ? <p>{error}</p> : null}
+      </form>
+    </section>
+  );
 };
