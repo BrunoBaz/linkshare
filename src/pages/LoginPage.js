93351a65c9863040159e328544fe477391125c54
ronaldherrera <ronaldcalzadilla31@gmail.com>
2022-06-06 22:12:24 +0200

    css-loginpage-registerpage-notfound

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,8 @@
 import { useContext, useState } from "react";
 import { logInUserService } from "../services/loginUserService";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import "./LoginRegisterPage.css";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
@@ -24,35 +25,42 @@ export const LoginPage = () => {
     }
   };
   return (
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleForm}>
-        <fieldset>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="pass">Password</label>
-          <input
-            type="password"
-            name="pass"
-            id="pass"
-            value={password}
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </fieldset>
+    <section className="body-login">
+      <section className="form-login">
+        <h1>Iniciar sesión</h1>
+        <form onSubmit={handleForm} className>
+          <fieldset className="input-login">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </fieldset>
+          <fieldset className="input-login">
+            <label htmlFor="pass">Password</label>
+            <input
+              type="password"
+              name="pass"
+              id="pass"
+              value={password}
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </fieldset>
 
-        <button>Login</button>
-        {error ? <p>{error}</p> : null}
-      </form>
+          {error ? <p>{error}</p> : null}
+          <section className="opciones-login">
+            <button className="boton-para-loguearse">entar</button>
+            <Link to="/register" className="register/login">
+              registrarse →
+            </Link>
+          </section>
+        </form>
+      </section>
     </section>
   );
 };
